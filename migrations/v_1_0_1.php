1388347514dd341a818d21ee51c59847131aa92f
Kilian <kilian@mirdan.de>
2015-08-26 13:12:39 +0200

    Separated text choice schema changes into separate migration file and increase version to 1.0.1-dev

---
+++ migrations/v_1_0_1.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ * @package hookup
+ * @copyright (c) 2015 kilianr
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace gn36\hookup\migrations;
+
+class v_1_0_1 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\gn36\hookup\migrations\v_1_0_0');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'add_columns' => array(
+				$this->table('hookup_dates') => array(
+					'text'			=> array('VCHAR:255', null),
+				),
+			),
+		);
+	}
+}
