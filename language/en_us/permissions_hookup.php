e284e663b6c6a94d20eb3c61d612b6264d92648e
Martin Beckmann <gn#36@phpbb.de>
2015-12-27 00:03:03 +0100

    Add language en_us
    Fixes #38

---
+++ language/en_us/permissions_hookup.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* permissions_hookup [English]
+*
+* @package language
+* @copyright (c) 2015 gn#36
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// Define categories and permission types
+$lang = array_merge($lang, array(
+	'ACL_F_HOOKUP'		=> 'Can add hookup meetings',
+));
